b352ee20cb9a560daf77a532e07cb2e13eba0c94
csavarese <chris.savarese@gmail.com>
2024-08-06 09:28:09 +0100

    tweaks

---
+++ Savheim4_Init_ONLY_ONCE.bat
@@ -0,0 +1,8 @@
+cd C:\Program Files (x86)\Steam\steamapps\common\Valheim
+git init
+git remote add origin https://github.com/Oosquai/SavheimIV.git
+git pull
+git reset --hard origin/main
+git checkout main
+pause
+exit
