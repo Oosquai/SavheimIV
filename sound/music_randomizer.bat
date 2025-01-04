75027b8bc6fae033df76bac6bba95df2240871f5
csavarese <chris.savarese@gmail.com>
2025-01-04 08:35:04 +0000

    fixes

--- sound/music_randomizer.bat
+++ sound/music_randomizer.bat
@@ -6,10 +6,30 @@
 ::echo %rand%
 echo TotinoTunes(tm) is now picking random epic music for you while your game loads!
 
-if %rand% LEQ 18 (playmp3.bat TSFH_Tyrianis_adj.mp3) else (
-if %rand% LEQ 36 (playmp3.bat TSFH_Flameheart_adj.mp3) else (
-if %rand% LEQ 54 (playmp3.bat TSFH_Black-Blade_adj.mp3) else (
-if %rand% LEQ 72 (playmp3.bat rage-sound_vortex_adj.mp3) else (
-if %rand% LEQ 90 (playmp3.bat timur-haisyn_brutal-fervour_adj.mp3) else (
-				  playmp3.bat Wind-Rose_Diggy-Diggy-Hole_adj.mp3) ) ) ) )
+if %rand% LEQ 30 (playmp3.bat Wind-Rose_Diggy-Diggy-Hole_A2.mp3) else (
+if %rand% LEQ 40 (playmp3.bat TSFH_Flameheart_A1.mp3) else (
+if %rand% LEQ 50 (playmp3.bat TSFH_Black-Blade_A2.mp3) else (
+if %rand% LEQ 60 (playmp3.bat TSFH_Tyrianis_A2.mp3) else (
+if %rand% LEQ 70 (playmp3.bat TSFH_Dragon-Rider_A1.mp3) else (
+if %rand% LEQ 80 (playmp3.bat TSFH_Magika_A1.mp3) else (
+if %rand% LEQ 90 (playmp3.bat rage-sound_vortex_A2.mp3) else (
+				  playmp3.bat Intro_A2.mp3) ) ) ) ) ) )
+
+
+REM playmp3.bat Wind-Rose_Diggy-Diggy-Hole_A2.mp3
+
+REM playmp3.bat TSFH_Flameheart_A1.mp3
+
+REM playmp3.bat Intro_A2.mp3
+
+REM playmp3.bat TSFH_Tyrianis_A2.mp3
+
+REM playmp3.bat TSFH_Dragon-Rider_A1.mp3
+
+REM playmp3.bat TSFH_Black-Blade_A2.mp3
+
+REM playmp3.bat rage-sound_vortex_A2.mp3
+
+REM playmp3.bat TSFH_Magika_A1.mp3
+
 exit
